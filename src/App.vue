7491a456a6f276643c286d1ce88fc0d151fccc7d
Belén <belen.munoz@usach.cl>
2025-02-17 15:45:38 -0300

    fix: reestructuración maquetación/layout

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,49 @@
 <template>
-    <div class="app_container">
+    <div class="layout">
       <Navbar v-if="!isAuthPage" />
-      <main class="main_content">
+      <Main :style="mainStyle">
         <router-view />
-      </main>
+      </Main>
     </div>
 </template>
   
 <script>
   import Navbar from "./components/Navbar.vue";
+  import Main from "./components/Main.vue";
+
   export default {
     name: "App",
     components: {
-      Navbar
+      Navbar,
+      Main
     },
     computed: {
       isAuthPage() {
         return this.$route.path === "/" || this.$route.path === "/signup";
+      },
+      mainStyle() {
+        return this.isAuthPage ? {} : { paddingTop: '60px', paddingBottom: '10px' };
       }
     }
   };
 </script>
+
+<style>
+.layout {
+    display: grid;
+    grid-template-columns: 1fr; /* Una sola columna */
+    grid-template-rows: auto 1fr; /* Navbar y Main */
+    grid-template-areas: 
+      "navbar"
+      "main";
+}
+
+.layout > Navbar {
+    grid-area: navbar;
+}
+
+.layout > Main {
+    grid-area: main;
+}  
+</style>
   
