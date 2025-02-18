ef7313b71823eb7f33925d0a3a57d420a7f52bef
Belén <belen.munoz@usach.cl>
2025-02-18 14:49:39 -0300

    fix: búsqueda considerando autenticación (token) y falta de cover

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -38,6 +38,7 @@
   <script setup>
   import { ref } from 'vue';
   import { useRouter } from 'vue-router';
+import apiClient from '../api/axios';
   
   const router = useRouter();
   const query = ref('');
@@ -53,13 +54,33 @@
     clearTimeout(debounceTimeout);
     debounceTimeout = setTimeout(async () => {
       try {
+        // Busqueda de libros en la API local
+        const token = localStorage.getItem("token"); // Obtener el token JWT almacenado
+        if (!token) {
+          console.error("No hay token de autenticación");
+          return;
+        }
+        const localResponse = await apiClient.get(`/books/search`,{
+          params: { query: query.value },
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        console.log(localResponse);
+        if (localResponse.data.lenght > 0){
+          books.value = localResponse.data.map(book => ({
+            id: book.id,
+            title: book.title,
+            cover: book.coverUrl || '/default-cover.jpg'
+          }));
+          return;
+        }
+        // Busqueda de libros en la API externa
         const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${query.value}&maxResults=5`);
         const data = await response.json();
   
         books.value = data.items?.map(book => ({
           id: book.id,
           title: book.volumeInfo.title,
-          cover: book.volumeInfo.imageLinks?.thumbnail || 'https://via.placeholder.com/50x75?text=No+Cover'
+          cover: book.volumeInfo.imageLinks?.thumbnail || '/default-cover.jpg'
         })) || [];
   
       } catch (error) {
@@ -69,10 +90,12 @@
   };
   
   const selectBook = (book) => {
+  if (router.currentRoute.value.path !== `/book/${book.id}`) {
     router.push(`/book/${book.id}`);
-    query.value = ''; // Limpia la barra de búsqueda después de seleccionar un libro
-    books.value = []; // Oculta la lista de sugerencias
-};
+  }
+  query.value = ''; // Limpia la barra de búsqueda
+  books.value = []; // Oculta la lista de sugerencias
+  };
 
   
   const logout = () => {
